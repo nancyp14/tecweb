84985eba3ce71a922b386031416c5a64c1b87406
Nan <tobonnancy14@gmail.com>
2025-10-05 21:28:24 -0600

    ícono de validación XHTML copiado a index.php

--- practicas/p08/index.php
+++ practicas/p08/index.php
@@ -155,9 +155,14 @@ echo "Versión de Apache y PHP: " . htmlspecialchars($_SERVER['SERVER_SOFTWARE']
 echo "Sistema operativo del servidor: " . PHP_OS . "<br />";
 echo "Idioma del navegador: " . htmlspecialchars($_SERVER['HTTP_ACCEPT_LANGUAGE']) . "<br />";
 echo "</div>";
-
 ?>
 
+<p>
+  <a href="https://validator.w3.org/check?uri=referer">
+    <img src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+  </a>
+</p>
+  
 
 </body>
 </html>
